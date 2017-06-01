6fe17c2eb960d43b6e9bba8783ab0d86aec04b36
Lívia Cavalcanti <livia.juliao@ccc.ufcg.edu.br>
2017-05-31 22:45:15 -0300

    passa nos testes

--- R03-02/src/main/java/sorting/linearSorting/CountingSort.java
+++ R03-02/src/main/java/sorting/linearSorting/CountingSort.java
@@ -1,63 +1,58 @@
 package sorting.linearSorting;
 
-import java.util.Arrays;
-
 import sorting.AbstractSorting;
 
 /**
- * Classe que implementa a estratégia de Counting Sort vista em sala. Procure
+ * Classe que implementa a estratÃ©gia de Counting Sort vista em sala. Procure
  * evitar desperdicio de memoria alocando o array de contadores com o tamanho
- * sendo o máximo inteiro presente no array a ser ordenado.
+ * sendo o mÃ¡ximo inteiro presente no array a ser ordenado.
  * 
  */
 public class CountingSort extends AbstractSorting<Integer> {
 
-	public static void main(String[] args) {
-		CountingSort tst = new CountingSort();
-		Integer[] array = { 4, 9, 3, 4, 0, 5, 1, 4 };
-		tst.sort(array, 0, array.length - 1);
+   public static void main(String[] args) {
+      CountingSort tst = new CountingSort();
+      Integer[] array = { 4, 9, 3, 4, 0, 5, 1, 4 };
+      tst.sort(array, 0, array.length - 1);
 
-	}
+   }
 
-	@Override
-	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		if (array.length != 0) {
-			int maiorElemento = encontraMaior(array, leftIndex, rightIndex);
-			Integer[] aux = new Integer[maiorElemento + 1];
-			Integer[] arrayFinal = new Integer[array.length];
+   @Override
+   public void sort(Integer[] array, int leftIndex, int rightIndex) {
+      if (array.length != 0) {
+         int maiorElemento = encontraMaior(array, leftIndex, rightIndex);
+         Integer[] aux = new Integer[maiorElemento + 1];
+         Integer[] arrayFinal = new Integer[array.length];
 
-			for (int i = 0; i < aux.length; i++) {
-				aux[i] = 0;
-			}
-			for (int i = leftIndex; i <= rightIndex; i++) {
-				aux[array[i]] = aux[array[i]] + 1;
-			}
+         for (int i = 0; i < aux.length; i++) {
+            aux[i] = 0;
+         }
+         for (int i = leftIndex; i <= rightIndex; i++) {
+            aux[array[i]] = aux[array[i]] + 1;
+         }
 
-			for (int i = 1; i < aux.length; i++)
-				aux[i] = aux[i] + aux[i - 1];
+         for (int i = 1; i < aux.length; i++)
+            aux[i] = aux[i] + aux[i - 1];
 
-			int atual = 0;
-			System.out.println(atual);
-			for (int i = rightIndex; i >= leftIndex; i--) {
-				arrayFinal[aux[array[i]]-1] = array[i];
-				aux[array[i]] = aux[array[i]] - 1;
-			}
-			for (int i = leftIndex; i <= rightIndex; i++) {
-				array[i] = arrayFinal[i];
-			}
-			System.out.println(Arrays.toString(array));
-		}
-	}
+         for (int i = rightIndex; i >= leftIndex; i--) {
+            arrayFinal[aux[array[i]] - 1] = array[i];
+            aux[array[i]] = aux[array[i]] - 1;
+         }
+         for (int i = leftIndex; i <= rightIndex; i++) {
+            array[i] = arrayFinal[i];
+         }
+      }
+   }
 
-	private int encontraMaior(Integer[] array, int leftIndex, int rightIndex) {
-		int maior = array[leftIndex];
+   private int encontraMaior(Integer[] array, int leftIndex, int rightIndex) {
+      int maior = array[leftIndex];
 
-		for (int i = leftIndex + 1; i <= rightIndex; i++) {
-			if (array[i] > maior)
-				maior = array[i];
-		}
+      for (int i = leftIndex + 1; i <= rightIndex; i++) {
+         if (array[i] > maior)
+            maior = array[i];
+      }
 
-		return maior;
-	}
+      return maior;
+   }
 
 }
--- R03-02/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
+++ R03-02/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
@@ -10,10 +10,66 @@ import sorting.AbstractSorting;
  */
 public class ExtendedCountingSort extends AbstractSorting<Integer> {
 
-	@Override
-	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+   public static void main(String[] args) {
+      ExtendedCountingSort tst = new ExtendedCountingSort();
+      Integer[] array = { 30, 28, 7, 29, 11, 26, 4, 22, 23, 31 };
+      tst.sort(array, 0, array.length - 1);
+
+   }
+
+   @Override
+   public void sort(Integer[] array, int leftIndex, int rightIndex) {
+      if (array.length != 0) {
+         int menor = procuraMenor(array, leftIndex, rightIndex);
+         int maior = procuraMaior(array, leftIndex, rightIndex);
+         Integer[] aux = new Integer[maior - menor + 1];
+         Integer[] arrayOrdenado = new Integer[rightIndex - leftIndex + 1];
+
+         for (int i = 0; i < aux.length; i++)
+            aux[i] = 0;
+
+         for (int i = leftIndex; i <= rightIndex; i++)
+            aux[array[i] - menor]++;
+
+         for (int i = 1; i < aux.length; i++)
+            aux[i] = aux[i] + aux[i - 1];
+
+         for (int i = rightIndex; i >= leftIndex; i--) {
+            arrayOrdenado[aux[array[i] - menor] - 1] = array[i];
+            aux[array[i] - menor]--;
+         }
+
+         int i = rightIndex;
+         int j = arrayOrdenado.length - 1;
+         while (i >= leftIndex && j >= 0) {
+
+            array[i] = arrayOrdenado[j];
+            i--;
+            j--;
+
+         }
+      }
+
+   }
+
+   private int procuraMaior(Integer[] array, int leftIndex, int rightIndex) {
+      int maior = array[leftIndex];
+
+      for (int i = leftIndex + 1; i <= rightIndex; i++)
+         if (array[i] > maior)
+            maior = array[i];
+
+      return maior;
+   }
+
+   private int procuraMenor(Integer[] array, int leftIndex, int rightIndex) {
+      int menor = array[leftIndex];
+
+      for (int i = leftIndex + 1; i < rightIndex; i++)
+         if (array[i] < menor)
+            menor = array[i];
+
+      return menor;
+   }
 
 }
